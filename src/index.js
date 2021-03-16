8935c26a6bd31ffc965ed351853639d41275591b
JCosta <josecostasantos.jr@gmail.com>
2021-03-16 15:12:44 -0300

    chore: HTTP Routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 
 const app = express();
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'});
+app.get('/course', (request, response) => {
+  return response.json(['Curso 1', 'Curso 2' , 'Curso 3']);
 });
+
+app.post('/course', (request, response) => {
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
+
+app.put('/course/:id', (request, response) => {
+  return response.json(['Curso 7', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
+
+app.patch('/course/:id', (request, response) => {
+  return response.json(['Curso 7', 'Curso 6', 'Curso 3', 'Curso 4']);
+});
+
+app.delete('/course/:id', (request, response) => {
+  return response.json(['Curso 6', 'Curso 3', 'Curso 4']);
+});
+
 app.listen(3333);
